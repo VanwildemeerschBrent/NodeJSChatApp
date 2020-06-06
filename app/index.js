8285c255f13ed1ec336c4f69dda7e0809162a3f7
BrentVanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-06-06 17:02:42 +0200

    Chat multiple users styling

--- app/index.js
+++ app/index.js
@@ -1,32 +1,61 @@
 const socket = io("http://localhost:3000");
 // Message form elements
 const messageForm = document.querySelector(".send_container");
-const messageInput = document.querySelector(".message_input");
 const messageContainer = document.querySelector(".message_container");
+const joinForm = document.querySelector(".join_chat");
 
-const userName = socket.on("chat-message", (message) => {
-  console.log("Message : ", message);
-  appendMessage(message);
+let userName = null;
+
+socket.on("chat-message", (message) => {
+  appendReceivedMessage(message);
+});
+
+socket.on("new-user", (userName) => appendNewUserJoined(userName));
+
+joinForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  userName = document.querySelector(".input_userName").value;
+  socket.emit("new-user", userName);
+  appendJoinedChat(userName);
+  document.querySelector(".popup_userName").classList += " hidden";
 });
 
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const message = messageInput.value;
-  socket.emit("send-chat-message", message);
-  appendOwnMessage(message);
-  messageInput.value = "";
+  const message = document.querySelector(".message_input").value;
+  appendSendMessage(message);
+  socket.emit("send-chat-message", `${userName} : ${message}`);
+  document.querySelector(".message_input").value = "";
 });
 
-const appendMessage = (message) => {
+const appendReceivedMessage = (message) => {
   const messageElement = document.createElement("div");
   messageElement.classList = "w-screen h-auto inline-block";
   messageElement.innerHTML = `<p class="bg-gray-600 w-auto max-w-md  h-auto text-white relative float-left p-2 rounded my-1 ml-3">${message}</p>`;
   messageContainer.append(messageElement);
+  messageElement.scrollIntoView();
 };
 
-const appendOwnMessage = (message) => {
+const appendSendMessage = (message) => {
   const htmlElement = document.createElement("div");
   htmlElement.classList = "w-screen h-auto inline-block";
-  htmlElement.innerHTML = `<p class="bg-blue-500 w-auto max-w-md h-auto text-white relative float-right p-2 rounded my-1 mr-3">${message}</p>`;
+  htmlElement.innerHTML = `<p class="bg-blue-500 w-auto max-w-md h-auto text-white relative float-right p-2 rounded my-1 mr-6">${message}</p>`;
   messageContainer.appendChild(htmlElement);
+  htmlElement.scrollIntoView();
+};
+
+const appendJoinedChat = () => {
+  const htmlElement = document.createElement("div");
+  htmlElement.classList = "w-screen h-auto inline-block";
+  htmlElement.innerHTML = `<p class="bg-green-500 w-auto max-w-md h-auto text-white relative float-right p-2 rounded my-1 mr-6">You have joined the chat</p>`;
+  messageContainer.appendChild(htmlElement);
+  htmlElement.scrollIntoView();
+};
+
+const appendNewUserJoined = (userName) => {
+  const htmlElement = document.createElement("div");
+  htmlElement.classList = "w-screen h-auto inline-block";
+  htmlElement.innerHTML = `<p class="bg-green-500 w-auto max-w-md h-auto text-white relative float-left p-2 rounded my-1 ml-3">${userName} has joined the chat</p>`;
+  messageContainer.appendChild(htmlElement);
+  htmlElement.scrollIntoView();
 };
--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const io = require("socket.io")(3000);
 
 io.on("connection", (socket) => {
   socket.on("send-chat-message", (message) => {
-    console.log(message);
     socket.broadcast.emit("chat-message", message);
+  });
+  socket.on("new-user", (userName) => {
+    console.log("User : ", userName);
+    socket.broadcast.emit("new-user", userName);
   });
 });
