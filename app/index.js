2ddbf16b7cfe10a29db144e86436e06d829902ba
BrentVanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-06-06 16:18:10 +0200

    Styling and layout of app

--- app/index.js
+++ app/index.js
@@ -1,5 +1,32 @@
 const socket = io("http://localhost:3000");
+// Message form elements
+const messageForm = document.querySelector(".send_container");
+const messageInput = document.querySelector(".message_input");
+const messageContainer = document.querySelector(".message_container");
 
-socket.on("message", (data) => {
-  console.log("Data", data);
+const userName = socket.on("chat-message", (message) => {
+  console.log("Message : ", message);
+  appendMessage(message);
 });
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  socket.emit("send-chat-message", message);
+  appendOwnMessage(message);
+  messageInput.value = "";
+});
+
+const appendMessage = (message) => {
+  const messageElement = document.createElement("div");
+  messageElement.classList = "w-screen h-auto inline-block";
+  messageElement.innerHTML = `<p class="bg-gray-600 w-auto max-w-md  h-auto text-white relative float-left p-2 rounded my-1 ml-3">${message}</p>`;
+  messageContainer.append(messageElement);
+};
+
+const appendOwnMessage = (message) => {
+  const htmlElement = document.createElement("div");
+  htmlElement.classList = "w-screen h-auto inline-block";
+  htmlElement.innerHTML = `<p class="bg-blue-500 w-auto max-w-md h-auto text-white relative float-right p-2 rounded my-1 mr-3">${message}</p>`;
+  messageContainer.appendChild(htmlElement);
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const io = require("socket.io")(3000);
 
 io.on("connection", (socket) => {
-  socket.emit("message", "Hello everyone");
+  socket.on("send-chat-message", (message) => {
+    console.log(message);
+    socket.broadcast.emit("chat-message", message);
+  });
 });
